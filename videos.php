3f92c323c8c4082f7caf1620651d8923b51a57c8
faithlifechurch <tcammack@faithlifechurch.org>
2018-04-03 16:36:38 -0400

    Added the foreach loop from Kirby Forum Support

--- videos.php
+++ videos.php
@@ -5,41 +5,36 @@
 <section class="section">
   <div class="container">
     <div class="columns">
-      <div class="column is-8 is-offset-2">
-
-
-		<?php if($videoURL): ?>
-
-		<?php $episode = $videos->findBy('video_id', $videoURL); ?>
-
-		<?php if($episode): ?>
+      <?php foreach($videos as $item): ?>
+        <div class="column is-8 is-offset-2">
 		  
 		  <?php if($item->video_type() === 'vimeo'): ?>
           		
 		  <div class="embed">
-		<iframe src="https://player.vimeo.com/video/<?= $episode->video_id();?>?api=1&color=ffffff&title=0&byline=0&portrait=0" width="640" height="360" frameborder="0" class="episode" webkitallowfullscreen mozallowfullscreen allowfullscreen ></iframe>
+		  <iframe src="https://player.vimeo.com/video/<?= $item->video_id();?>?api=1&color=ffffff&title=0&byline=0&portrait=0" width="640" height="360" frameborder="0" class="episode" webkitallowfullscreen mozallowfullscreen allowfullscreen ></iframe>
 		</div>
 
-		<h1 class="is-size-2 is-size-4-mobile mt10"><?= $episode->name() ?></h1>
+		<h1 class="is-size-2 is-size-4-mobile mt10"><?= $item->name() ?></h1>
 		  
 		  
 		<?php endif; ?>
 		<?php if($item->video_type() === 'youtube'): ?>
 		  
-		  <?= "Hey, I'm a You Tube video" ?>
 		  
   			<div class="embed">
-			<iframe src="https://www.youtube.com/watch?v=<?= $episode->video_id();?>?api=1&color=ffffff&title=0&byline=0&portrait=0" width="640" height="360" frameborder="0" class="episode" webkitallowfullscreen mozallowfullscreen allowfullscreen ></iframe>
+			<iframe src="https://www.youtube.com/watch?v=<?= $item->video_id();?>?api=1&color=ffffff&title=0&byline=0&portrait=0" width="640" height="360" frameborder="0" class="episode" webkitallowfullscreen mozallowfullscreen allowfullscreen ></iframe>
 						</div>
 
-						<h1 class="is-size-2 is-size-4-mobile mt10"><?= $episode->name() ?></h1>
+						<h1 class="is-size-2 is-size-4-mobile mt10"><?= $item->name() ?></h1>
 		  
 		  <?php endif; ?>
 
-</div>
+      </div>
+    <?php endforeach ?>
     </div>
   </div>
 </section>
+
 
 
 <?php snippet('footer') ?>
